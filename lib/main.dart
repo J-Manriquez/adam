7702b41ef64499e3c2656941a327d183c1652e69
jorge.anttoonio <jorge.anttoonio@gmail.com>
2024-09-30 07:18:07 +0000

    flecha back en drawer

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,11 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => ThemeModel()), // Proveedor de colores
-        ChangeNotifierProvider(create: (context) => FontSizeModel()), // Proveedor de tamaños de fuente
+        ChangeNotifierProvider(
+            create: (context) => ThemeModel()), // Proveedor de colores
+        ChangeNotifierProvider(
+            create: (context) =>
+                FontSizeModel()), // Proveedor de tamaños de fuente
       ],
       child: MyApp(),
     ),
@@ -23,8 +26,6 @@ void main() {
 class MyApp extends StatelessWidget {
   // Instancia del logger
   final CustomLogger customLogger = CustomLogger();
-  
-
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +33,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, // Oculta el banner de depuración
       title: 'ADAM', // Define el título de la aplicación
       theme: ThemeData(
-        primarySwatch: Colors.blue, // Establece el tema principal de la app (colores)
+        primarySwatch:
+            Colors.blue, // Establece el tema principal de la app (colores)
       ),
       home: MainScreen(customLogger: customLogger), // Pasar customLogger
     );
@@ -41,8 +43,9 @@ class MyApp extends StatelessWidget {
 
 class MainScreen extends StatefulWidget {
   final CustomLogger customLogger; // Agregar este campo
-  const MainScreen({super.key, required this.customLogger}); // Asegúrate de requerir el parámetro
-
+  const MainScreen(
+      {super.key,
+      required this.customLogger}); // Asegúrate de requerir el parámetro
 
   @override
   State<MainScreen> createState() => _MainScreenState();
@@ -51,7 +54,8 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 1; // Índice seleccionado inicialmente (1 = HomeScreen)
   final List<Widget> _pages = [];
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>(); // Clave global para controlar el Scaffold
+  final GlobalKey<ScaffoldState> _scaffoldKey =
+      GlobalKey<ScaffoldState>(); // Clave global para controlar el Scaffold
 
   @override
   void initState() {
@@ -71,13 +75,16 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final themeModel = Provider.of<ThemeModel>(context); // Obtenemos el modelo de tema
-    final fontSizeModel = Provider.of<FontSizeModel>(context); // Obtenemos el modelo de tamaño de fuente
+    final themeModel =
+        Provider.of<ThemeModel>(context); // Obtenemos el modelo de tema
+    final fontSizeModel = Provider.of<FontSizeModel>(
+        context); // Obtenemos el modelo de tamaño de fuente
 
     return Scaffold(
       key: _scaffoldKey, // Asigna el Scaffold al GlobalKey
       appBar: AppBar(
-        backgroundColor: themeModel.primaryButtonColor, // Color dinámico para el AppBar
+        backgroundColor:
+            themeModel.primaryButtonColor, // Color dinámico para el AppBar
         // Personaliza el ícono de menú (drawer) con el tamaño que quieras
         leading: IconButton(
           icon: Icon(
@@ -93,50 +100,85 @@ class _MainScreenState extends State<MainScreen> {
         title: Text(
           'Asistente Digital',
           style: TextStyle(
-            fontSize: fontSizeModel.titleSize, // Tamaño dinámico del texto del título
+            fontSize:
+                fontSizeModel.titleSize, // Tamaño dinámico del texto del título
             color: themeModel.primaryTextColor, // Color dinámico del texto
           ),
         ),
       ),
       drawer: Drawer(
-        backgroundColor: themeModel.backgroundColor, // Color dinámico para el Drawer
+        backgroundColor:
+            themeModel.backgroundColor, // Color dinámico para el Drawer
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
               decoration: BoxDecoration(
-                color: themeModel.primaryButtonColor, // Color dinámico en el header del Drawer
+                color: themeModel
+                    .primaryButtonColor, // Color dinámico en el header del Drawer
               ),
-              child: Text(
-                'Menú principal',
-                style: TextStyle(
-                  color: themeModel.primaryTextColor, // Color dinámico del texto
-                  fontSize: fontSizeModel.titleSize, // Tamaño dinámico del texto
-                ),
+              child: Row(
+                // Alineación horizontal de los elementos dentro de la fila
+                mainAxisAlignment: MainAxisAlignment
+                    .start, // Centra horizontalmente el contenido
+                // Alineación vertical de los elementos dentro de la fila
+                crossAxisAlignment: CrossAxisAlignment
+                    .start, // Centra verticalmente el contenido
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back,
+                        color: themeModel.primaryIconColor),
+                    iconSize:
+                        fontSizeModel.iconSize, // Tamaño dinámico del ícono
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Cierra el Drawer
+                    },
+                  ),
+                  SizedBox(width: 8), // Espacio entre el ícono y el texto
+                  Text(
+                    'Menú principal',
+                    style: TextStyle(
+                      color: themeModel
+                          .primaryTextColor, // Color dinámico del texto
+                      fontSize:
+                          fontSizeModel.titleSize, // Tamaño dinámico del texto
+                    ),
+                  ),
+                ],
               ),
             ),
             ListTile(
-              leading: Icon(Icons.settings, size: fontSizeModel.iconSize, color: themeModel.secondaryIconColor), // Ícono con tamaño dinámico
+              leading: Icon(Icons.settings,
+                  size: fontSizeModel.iconSize,
+                  color: themeModel
+                      .secondaryIconColor), // Ícono con tamaño dinámico
               title: Text(
                 'Configuraciones',
                 style: TextStyle(
-                  color: themeModel.secondaryTextColor, // Color dinámico del texto
+                  color:
+                      themeModel.secondaryTextColor, // Color dinámico del texto
                   fontSize: fontSizeModel.textSize, // Tamaño dinámico del texto
                 ),
               ),
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const SettingsScreen()), // Navega a SettingsScreen
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const SettingsScreen()), // Navega a SettingsScreen
                 );
               },
             ),
             ListTile(
-              leading: Icon(Icons.info, size: fontSizeModel.iconSize, color: themeModel.secondaryIconColor), // Ícono con tamaño dinámico
+              leading: Icon(Icons.info,
+                  size: fontSizeModel.iconSize,
+                  color: themeModel
+                      .secondaryIconColor), // Ícono con tamaño dinámico
               title: Text(
                 'Acerca de',
                 style: TextStyle(
-                  color: themeModel.secondaryTextColor, // Color dinámico del texto
+                  color:
+                      themeModel.secondaryTextColor, // Color dinámico del texto
                   fontSize: fontSizeModel.textSize, // Tamaño dinámico del texto
                 ),
               ),
@@ -149,39 +191,48 @@ class _MainScreenState extends State<MainScreen> {
       ),
       body: Container(
         color: themeModel.backgroundColor, // Color de fondo dinámico
-        child: _pages[_selectedIndex], // Cambia la página según el índice seleccionado
+        child: _pages[
+            _selectedIndex], // Cambia la página según el índice seleccionado
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.warning, size: fontSizeModel.iconSize), // Ícono con tamaño dinámico
+            icon: Icon(Icons.warning,
+                size: fontSizeModel.iconSize), // Ícono con tamaño dinámico
             label: 'SOS',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home, size: fontSizeModel.iconSize), // Ícono con tamaño dinámico
+            icon: Icon(Icons.home,
+                size: fontSizeModel.iconSize), // Ícono con tamaño dinámico
             label: 'Inicio',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.notifications, size: fontSizeModel.iconSize), // Ícono con tamaño dinámico
+            icon: Icon(Icons.notifications,
+                size: fontSizeModel.iconSize), // Ícono con tamaño dinámico
             label: 'Recordatorios',
           ),
         ],
         currentIndex: _selectedIndex, // Índice seleccionado
-        selectedItemColor: themeModel.secondaryButtonColor, // Color dinámico del ítem seleccionado
-        unselectedItemColor: themeModel.primaryIconColor, // Color para ítems no seleccionados
-        backgroundColor: themeModel.primaryButtonColor, // Color de fondo dinámico
+        selectedItemColor: themeModel
+            .secondaryButtonColor, // Color dinámico del ítem seleccionado
+        unselectedItemColor:
+            themeModel.primaryIconColor, // Color para ítems no seleccionados
+        backgroundColor:
+            themeModel.primaryButtonColor, // Color de fondo dinámico
         showUnselectedLabels: true,
-        
         // Modifica el tamaño del texto de los labels
         selectedLabelStyle: TextStyle(
-          fontSize: fontSizeModel.textSize, // Tamaño dinámico del texto seleccionado
+          fontSize:
+              fontSizeModel.textSize, // Tamaño dinámico del texto seleccionado
           color: themeModel.secondaryIconColor, // Color del texto seleccionado
         ),
         unselectedLabelStyle: TextStyle(
-          fontSize: fontSizeModel.textSize - 2, // Tamaño dinámico del texto no seleccionado
+          fontSize: fontSizeModel.textSize -
+              2, // Tamaño dinámico del texto no seleccionado
           color: themeModel.primaryIconColor, // Color del texto no seleccionado
         ),
-        onTap: _onItemTapped, // Método que se llama cuando se selecciona un ítem
+        onTap:
+            _onItemTapped, // Método que se llama cuando se selecciona un ítem
       ),
     );
   }
